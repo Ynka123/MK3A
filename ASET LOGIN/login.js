75ea3b922c411cb0cb522af98aa84255ee243047
Ynka123 <541231219@student.smktelkom-pwt.sch.id>
2024-09-04 10:23:36 +0700

    add fitur login

---
+++ ASET LOGIN/login.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    const correctUsername = 'Yunika';
+    const correctPassword = 'admin#1234';
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    if(username === correctUsername && password === correctPassword){
+        alert("login berhasil");
+        window.location.href = 'index.html';
+    }else{
+        alert("username atau password tidak sesuai");
+    }
+    
+    });
